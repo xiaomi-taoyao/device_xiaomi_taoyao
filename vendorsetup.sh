d54dbc65fe84e284fd0e2cf1bb519bb68c4706b9
najieb <Najiebgretz12@gmail.com>
2023-10-17 12:15:58 +0700

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -3,16 +3,16 @@ echo 'Starting to clone stuffs needed to build for taoyao'
 
 # Vendor
 echo 'Cloning vendor tree'
-rm -rf vendor/xiaomi/taoyao && git clone git@github.com:xiaomi-taoyao/vendor_xiaomi_taoyao.git vendor/xiaomi/taoyao
+rm -rf vendor/xiaomi/taoyao && git clone https://github.com/xiaomi-taoyao/vendor_xiaomi_taoyao.git vendor/xiaomi/taoyao
 
 
 # Kernel
 echo 'Cloning kernel tree'
-rm -rf device/xiaomi/taoyao-kernel && git clone git@github.com:xiaomi-taoyao/device_xiaomi_taoyao-kernel.git device/xiaomi/taoyao-kernel
+rm -rf device/xiaomi/taoyao-kernel && git clone https://github.com/xiaomi-taoyao/device_xiaomi_taoyao-kernel.git device/xiaomi/taoyao-kernel
 
 # Kernel
 echo 'Cloning kernel header'
-rm -rf kernel/xiaomi/sm8350 && git clone git@github.com:xiaomi-taoyao/kernel_xiaomi_sm8350.git kernel/xiaomi/sm8350
+rm -rf kernel/xiaomi/sm8350 && git clone https://github.com/xiaomi-taoyao/kernel_xiaomi_sm8350.git kernel/xiaomi/sm8350
 
 
 # Gapps
@@ -26,7 +26,7 @@ rm -rf kernel/xiaomi/sm8350 && git clone git@github.com:xiaomi-taoyao/kernel_xia
 
 # Hardware Xiaomi
 echo 'Cloning Hardware Xiaomi'
-rm -rf hardware/xiaomi && git clone git@github.com:xiaomi-taoyao/hardware_xiaomi.git hardware/xiaomi
+rm -rf hardware/xiaomi && git clone https://github.com/xiaomi-taoyao/hardware_xiaomi.git hardware/xiaomi
 
 
 echo 'delete vendorsetup.sh from device tree once this is done'
