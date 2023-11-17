849918278698224e78bf1a926bbf6ad2895b1518
Dmitry <42121541+dkpost3@users.noreply.github.com>
2023-11-17 07:25:56 +0300

    Add files via upload

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -14,19 +14,17 @@ rm -rf device/xiaomi/taoyao-kernel && git clone git@github.com:xiaomi-taoyao/dev
 echo 'Cloning kernel header'
 rm -rf kernel/xiaomi/sm8350 && git clone git@github.com:xiaomi-taoyao/kernel_xiaomi_sm8350.git kernel/xiaomi/sm8350
 
-
 # Gapps
 #echo 'Cloning gapps'
 #rm -rf vendor/gms && rm -rf vendor/gapps &&  git clone --depth=1 https://gitlab.com/vherawidatama/vendor_gms_mod.git vendor/gapps
 
-
 # Camera
-#echo 'Cloning Leica Camera'
-#git clone https://gitlab.com/vherawidatama/proprietary_vendor_xiaomi_camera.git -b topaz-leica vendor/xiaomi/camera
+echo 'Cloning Leica Camera'
+git clone https://gitlab.com/vherawidatama/proprietary_vendor_xiaomi_camera.git -b udc vendor/xiaomi/camera
 
 # Hardware Xiaomi
-echo 'Cloning Hardware Xiaomi'
-rm -rf hardware/xiaomi && git clone git@github.com:xiaomi-taoyao/hardware_xiaomi.git hardware/xiaomi
+#echo 'Cloning Hardware Xiaomi'
+#rm -rf hardware/xiaomi && git clone git@github.com:xiaomi-taoyao/hardware_xiaomi.git hardware/xiaomi
 
 
 echo 'delete vendorsetup.sh from device tree once this is done'
